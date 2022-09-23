fa1bbb945e401a278c0483f31f312fa4e8c910e8
mikeyeyer098 <mikeyeyer098@gmail.com>
2022-09-23 10:56:25 -0600

    added tab shortcuts for pane control

--- lua/mikey_std/remap.lua
+++ lua/mikey_std/remap.lua
@@ -3,3 +3,6 @@ local nnoremap = require("mikey_std.keymap").nnoremap
 nnoremap("<leader>e", "<cmd>Ex<CR>")
 nnoremap("<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>")
 nnoremap("<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>")
+nnoremap("<leader><Tab>", "<C-w>w")
+nnoremap("<leader>v<Tab>", "<C-w>v")
+nnoremap("<leader>c<Tab>", "<C-w>c")
